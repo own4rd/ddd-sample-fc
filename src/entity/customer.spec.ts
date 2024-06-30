b6fbb1046b880f698ee42d2b8c7526fbcea18793
Wagner <wagnerpalmeira.ti@gmail.com>
2024-06-30 01:21:15 -0300

    Adding test for add rewards

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -46,4 +46,14 @@ describe("Customer unit tests", () => {
             customer.activate();
         }).toThrow("Address is required");
     });
+
+    it("should add reward points", () => {
+        const customer = new Customer("1", "Customer 1");
+        expect(customer.rewardPoints).toBe(0);
+
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(10);
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(20);
+    });
 });

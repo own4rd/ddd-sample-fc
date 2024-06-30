06323a440e21e68c8fb53940352f7cd27708e747
Wagner <wagnerpalmeira.ti@gmail.com>
2024-06-30 00:42:28 -0300

    Solving jest problem of tsc check

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("Rua dois", 2, "12345-678", "São Paulo");
 customer.changeAddress(address);
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 30);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 4);
+const item2 = new OrderItem("2", "Item 2", 30, "p2", 5);
 const order = new Order("1", "123", [item1, item2]);
